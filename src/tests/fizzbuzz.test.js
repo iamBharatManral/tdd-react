43644e53266ee57d3b46069dc00cc443d01ee677
iamBharatManral <justbharatmanral@gmail.com>
2024-01-21 17:51:00 +0530

    feat: fizzbuzz using tdd approach

---
+++ src/tests/fizzbuzz.test.js
@@ -0,0 +1,19 @@
+const fizzBuzz = require('../fizz-buzz/fizzbuzz')
+describe('fizzBuzz', () => {
+    it('returns "FizzBuzz" for multiples of 3 and 5', () => {
+        expect(fizzBuzz(15)).toBe('FizzBuzz')
+        expect(fizzBuzz(30)).toBe('FizzBuzz')
+    })
+    it('returns "Fizz" for multiples of 3', () => {
+        expect(fizzBuzz(3)).toBe('Fizz')
+        expect(fizzBuzz(12)).toBe('Fizz')
+    })
+    it('returns "Buzz" for multiples of 5', () => {
+        expect(fizzBuzz(5)).toBe('Buzz')
+        expect(fizzBuzz(10)).toBe('Buzz')
+    })
+    it('returns number itself for neither multiples of "3", "5", or "3 and 5"', () => {
+        expect(fizzBuzz(1)).toBe(1)
+        expect(fizzBuzz(2)).toBe(2)
+    })
+})
